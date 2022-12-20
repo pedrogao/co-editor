40e9282b91171b67740ef70f9fa28725a718bc50
pedrogao <1312342604@qq.com>
2022-12-20 22:53:29 +0800

    fix: peritext

--- examples/peritext/vite.config.ts
+++ examples/peritext/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
+  publicDir: "static",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        essay: resolve(__dirname, "essay/index.html"),
+      },
+    },
+  },
 });
